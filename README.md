e91ebf2cfaa68e2d84e67a14f655902e42bbcb9f
Nik Skoufis <nskoufis@seek.com.au>
2024-03-12 10:42:19 +1100

    Initial commit with some setup info

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # kubernetes-from-first-principles
+
+## What is this?
+
 TODO
+
+## Setup
+
+The exercises in this repo require `kubectl` and `kind`.
+Optionally `krew` can be used to manage `kubectl` plugins.
+
+Install these tools from official sources, or you can use the Nix flake in the root of this repo.
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A Nix-flake-based Kubernetes playground";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [
+        (self: super: rec {
+          kind = super.kind;
+          kubectl = super.kubectl;
+          krew = super.krew;
+        })
+      ];
+      pkgs = import nixpkgs { inherit overlays system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ kind kubectl krew ];
+
+        shellHook = ''
+        '';
+      };
+    });
+}
